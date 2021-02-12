8706dcd7f13ea8841851dbe3cd0e7570417b4d2f
alexLaws <tal1286@gmail.com>
2021-02-11 20:52:58 -0800

    first try 26

---
+++ 026problem.py
@@ -0,0 +1,26 @@
+# A unit fraction contains 1 in the numerator. The decimal representation of the unit fractions with denominators 2 to 10 are given:
+
+# 1/2	= 	0.5
+# 1/3	= 	0.(3)
+# 1/4	= 	0.25
+# 1/5	= 	0.2
+# 1/6	= 	0.1(6)
+# 1/7	= 	0.(142857)
+# 1/8	= 	0.125
+# 1/9	= 	0.(1)
+# 1/10	= 	0.1
+# Where 0.1(6) means 0.166666..., and has a 1-digit recurring cycle. It can be seen that 1/7 has a 6-digit recurring cycle.
+
+# Find the value of d < 1000 for which 1/d contains the longest recurring cycle in its decimal fraction part.
+
+longest = 0
+
+for x in range(1,1000):
+    test = str(1/x)[2:]
+    for n in range(int(len(test)/2)):
+        if test[:n] == test[n:2*n]:
+            if n > longest:
+                print(test[:n])
+                print(test[n:2*n])
+                print(x)
+                longest = x
